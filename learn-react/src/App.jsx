cca47650eeb6c6fa0a7e758ebd188b7d8f0acd34
minomad <93kymins@gmail.com>
2023-08-09 14:41:35 +0900

    컴포넌트 학습

---
+++ learn-react/src/App.jsx
@@ -0,0 +1,14 @@
+import List from './List';
+import Heading from './Heading';
+import Description from './Description';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Description />
+      <List />
+    </div>
+  );
+}
+export default App;
--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -1,15 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
-
-reactDomRoot.render(
-  <div>
-    <h1>React</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ab animi, labore facilis obcaecati ullam placeat molestiae. Enim, consectetur laudantium.</p>
-    <ul>
-      <li>list item A</li>
-      <li>list item B</li>
-      <li>list item C</li>
-    </ul>
-  </div>
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>
 );
