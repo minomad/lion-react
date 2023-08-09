644858b085f1936fb6b67c2f4903ddc32d7c6ae9
minomad <93kymins@gmail.com>
2023-08-09 16:17:44 +0900

    컴포넌트 학습

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,9 +1,11 @@
 import RootLayout from './layout/RootLayout';
 import Home from './pages/Home';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="App">
+      <Logo />
       <RootLayout>
         <Home />
       </RootLayout>
