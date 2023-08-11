7dca08ce5dc98370b7fd9e31168eae4149618c8c
minomad <93kymins@gmail.com>
2023-08-11 16:24:06 +0900

    CSS 모듈 학습

--- learn-react/src/pages/Practice/FilterableList.jsx
+++ learn-react/src/pages/Practice/FilterableList.jsx
@@ -1,16 +1,23 @@
-import styleModule from '@/styles/FilterableList.module.css'
+import styles from '@/styles/FilterableList.module.css';
+
 
 function FilterableList() {
   return (
     <>
-    <form>
-      <div>
-        <label htmlFor="todo"></label>
-        <input id="todo" type="text" placeholder="휴일에 할 일"/>
-      </div>
-      <button type="submit">추가</button>
-    </form>
+      <form>
+        <div>
+          <label htmlFor="todo"></label>
+          <input
+            id="todo"
+            type="text"
+            placeholder="휴일에 할 일"
+            className={styles.primary}
+          />
+        </div>
+        <button type="submit">추가</button>
+      </form>
     </>
   )
 }
+
 export default FilterableList
--- learn-react/vite.config.js
+++ learn-react/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { env } from 'process'
+
+const idDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css:{
-    devSourcemap:true
+    devSourcemap:true,
+    modules:{
+      generateScopedName: idDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
+    }
   },
   resolve: {
     // alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
