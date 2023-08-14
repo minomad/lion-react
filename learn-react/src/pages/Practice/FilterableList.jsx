3a3b91f75e58af51950749104c64928b4804c608
minomad <93kymins@gmail.com>
2023-08-14 12:48:11 +0900

    사이드 이펙트 학습

--- learn-react/src/pages/Practice/FilterableList.jsx
+++ learn-react/src/pages/Practice/FilterableList.jsx
@@ -1,17 +1,17 @@
 import styles from '@/styles/FilterableList.module.css';
-
+import SideEffect from '@/components/SideEffect';
 
 function FilterableList() {
   return (
     <>
+      <SideEffect />
       <form>
-        <div>
+        <div className={styles.accent}>
           <label htmlFor="todo"></label>
           <input
             id="todo"
             type="text"
             placeholder="휴일에 할 일"
-            className={styles.primary}
           />
         </div>
         <button type="submit">추가</button>
