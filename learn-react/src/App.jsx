09b9883e3dc6c36b9120f2e7a1f3e1c2f0c3d229
minomad <93kymins@gmail.com>
2023-08-18 15:54:20 +0900

    라우터 학습

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,15 +1,85 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
-import LearnStateAndEffects from './pages/LearnStateAndEffects';
-import ProductList from './components/ProductList';
+
+// const Nav = () => (
+//   <nav>
+//     <ul className="flex gap-4 p-5 font-extralight bg-slate-50/50 shadow-lg">
+//       <li>
+//         <NavLink
+//           to="/"
+//           className={({ isActive }) =>
+//             isActive ? 'font-semibold text-rose-600' : ''
+//           }
+//         >
+//           Home
+//         </NavLink>
+//       </li>
+//       <li>
+//         <NavLink
+//           to="/products"
+//           className={({ isActive }) =>
+//             isActive ? 'font-semibold text-rose-600' : ''
+//           }
+//         >
+//           Products
+//         </NavLink>
+//       </li>
+//       <li>
+//         <NavLink
+//           to="/contact"
+//           className={({ isActive }) =>
+//             isActive ? 'font-semibold text-rose-600' : ''
+//           }
+//         >
+//           Contact
+//         </NavLink>
+//       </li>
+//     </ul>
+//   </nav>
+// );
+
+const Home = () => (
+  <div>
+    <Nav />
+    <h1 className="text-emerald-500">Home</h1>
+  </div>
+);
+const Products = () => (
+  <div>
+    <Nav />
+    <h1 className="text-emerald-500">Products</h1>
+  </div>
+);
+const Contact = () => (
+  <div>
+    <Nav />
+    <h1 className="text-emerald-500">Contact</h1>
+  </div>
+);
+
+// Router Object
+// 경로(path), 요소(element ← <Component />)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    //중첩된 라우트
+    chlidren: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+]);
+
+console.log(router);
 
 function App() {
   return (
     <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-        <ProductList />
-      </RootLayout>
+      <RouterProvider router={router} />
     </div>
   );
 }
+
 export default App;
