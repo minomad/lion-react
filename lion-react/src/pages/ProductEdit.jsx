316b5da25261e067317fede48d62dcc3d9cdfcba
minomad <93kymins@gmail.com>
2023-08-21 15:51:02 +0900

    `

--- lion-react/src/pages/ProductEdit.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-function ProductEdit() {
-  const { productId } = useParams();
-
-  return <div>ProductEdit {productId} </div>;
-}
-
-export default ProductEdit;
