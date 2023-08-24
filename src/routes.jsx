7da21f4d301871b7f2c7db11421839f0e5043b7a
minomad <93kymins@gmail.com>
2023-08-24 10:19:04 +0900

    ref 학습

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,6 +10,10 @@ import PassingProps from './learn/1-passing-props';
 import LiftingStateUp from './learn/2-lifting-state-up';
 import PropsDrillingIssue from './learn/3-props-drilling-issue';
 import ReactContextAPI from './learn/4-react-context-api';
+import RefExampleMemoValues from './learn/5-ref-1-memo-values';
+import RefExampleReferencingDOM from './learn/6-ref-2-referencing-dom';
+import GSAP_Animation from './learn/7-ref-3-gsap-animation';
+import FramerMotion_Animation from './learn/8-framer-motion-declaration-animation';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +29,10 @@ const router = createBrowserRouter(
       <Route path="learn/02" element={<LiftingStateUp />} />
       <Route path="learn/03" element={<PropsDrillingIssue />} />
       <Route path="learn/04" element={<ReactContextAPI />} />
+      <Route path="learn/05" element={<RefExampleMemoValues />} />
+      <Route path="learn/06" element={<RefExampleReferencingDOM />} />
+      <Route path="learn/07" element={<GSAP_Animation />} />
+      <Route path="learn/08" element={<FramerMotion_Animation />} />
     </Route>
   )
 );
