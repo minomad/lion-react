ae143f710dc816db56effbf326314a5ba313b81e
minomad <93kymins@gmail.com>
2023-08-24 15:17:46 +0900

    인증

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,7 +4,6 @@ import pb from '@/api/pocketbase';
 import debounce from '@/utils/debounce';
 
 function SignIn() {
-
   const navigate = useNavigate();
 
   const [formState, setFormState] = useState({
@@ -17,28 +16,27 @@ function SignIn() {
 
     const { email, password } = formState;
 
-    // PocketBase SDK 인증(로그인) 요청
-    const authData = await pb.collection('users').authWithPassword(email, password);
-
-    console.log(authData);
+    await pb.collection('users').authWithPassword(email, password);
 
     navigate('/');
   };
 
-  const handleInput = (e) => {
+  const handleInput = debounce((e) => {
     const { name, value } = e.target;
     setFormState({
       ...formState,
-      [name]: value
+      [name]: value,
     });
-  };
-  const handleDebounceInput = debounce(handleInput, 500);
-  
+  }, 400);
+
   return (
-    <div className='flex flex-col items-center'>
+    <div>
       <h2>로그인 폼</h2>
 
-      <form onSubmit={handleSignIn} className='flex flex-col gap-2 mt-2 justify-start items-start'>
+      <form
+        onSubmit={handleSignIn}
+        className="flex flex-col gap-2 mt-2 justify-start items-start"
+      >
         <div>
           <label htmlFor="email">이메일</label>
           <input
@@ -46,8 +44,8 @@ function SignIn() {
             name="email"
             id="email"
             defaultValue={formState.email}
-            onChange={handleDebounceInput}
-            className='border border-slate-300 ml-2'
+            onChange={handleInput}
+            className="border border-slate-300 ml-2"
           />
         </div>
         <div>
@@ -57,17 +55,39 @@ function SignIn() {
             name="password"
             id="password"
             defaultValue={formState.password}
-            onChange={handleDebounceInput}
-            className='border border-slate-300 ml-2'
+            onChange={handleInput}
+            className="border border-slate-300 ml-2"
           />
         </div>
-        <div className='flex gap-2'>
-          <button type="submit" className='disabled:cursor-not-allowed'>로그인</button>
+        <div className="flex gap-2">
+          <button type="submit" className="disabled:cursor-not-allowed">
+            로그인
+          </button>
           <button type="reset">취소</button>
         </div>
       </form>
 
       <Link to="/signup">회원가입</Link>
+      <button
+        type="button"
+        className='ml-4'
+        onClick={async () => {
+          if (confirm('뭐가 맘에 안드시죠? 정말 탈퇴할 생각인가요?')) {
+            if (pb.authStore.model) {
+              try {
+                await pb.collection('users').delete(pb.authStore.model.id);
+                console.log('탈퇴 성공');
+              } catch (error) {
+                console.error(error);
+              }
+            } else {
+              console.log('현재 로그인 된 사용자가 없어요.');
+            }
+          }
+        }}
+      >
+        탈퇴
+      </button>
     </div>
   );
 }
